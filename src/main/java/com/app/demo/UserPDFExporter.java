d6911990304925a747d27b2f4d3b5059d0bed689
Ganapathy Subramanian S <60843507+GanapathySubramanian@users.noreply.github.com>
2021-12-23 16:08:59 +0530

    Update UserPDFExporter.java

--- src/main/java/com/app/demo/UserPDFExporter.java
+++ src/main/java/com/app/demo/UserPDFExporter.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.app.demo.model.Booking;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 
@@ -18,57 +19,12 @@ import com.lowagie.text.pdf.PdfWriter;
  
 public class UserPDFExporter {
  
-   
-	private int booking_id;
-	private String user_email;
-	private String first_name;
-	private String last_name;
-	private String user_phone;
-	private String user_gender;
-	private String user_address;
-	private String hotel_name;
-	private int hotel_price;
-	private String cater_name;
-	private int cater_price;
-	private int total_price;
-	private String booked_on;
-	private String deco_name_desc;
-	private String dj_name_desc;
-	private String makeup_name_desc;
-	private String photo_name_desc;
-	private String event_name;
-	private Date event_date;
-	private String max_hour;
-	private String no_guest;
+	private Booking booking;
 
-	public UserPDFExporter(int id, String email, String firstName, String lastName, String contactno, String gender,
-			String address, String hotelName, int hotelPrice, String caterName, int caterPrice, int total_amount,
-			String booked_date, String decorator_name_desc, String dj_name_desc, String makeupartist_name_desc,
-			String photographer_name_desc, String eventname, Date eventdate, String max_total_hour,
-			String no_of_guest) {
+	public UserPDFExporter(Booking booking) {
 		// TODO Auto-generated constructor stub
-		this.booking_id=id;
-		this.user_email=email;
-		this.first_name=firstName;
-		this.last_name=lastName;
-		this.user_phone=contactno;
-		this.user_gender=gender;
-		this.user_address=address;
-		this.hotel_name=hotelName;
-		this.hotel_price=hotelPrice;
-		this.cater_name=caterName;
-		this.cater_price=caterPrice;
-		this.total_price=total_amount;
-		this.booked_on=booked_date;
-		this.deco_name_desc=decorator_name_desc;
-		this.dj_name_desc=dj_name_desc;
-		this.makeup_name_desc=makeupartist_name_desc;
-		this.photo_name_desc=photographer_name_desc;
-		this.event_name=eventname;
-		this.event_date=eventdate;
-		this.max_hour=max_total_hour;
-		this.no_guest=no_of_guest;
 		
+		this.booking=booking;
 		
 	}
 
@@ -94,89 +50,97 @@ public class UserPDFExporter {
         parafont.setSize(10);
         parafont.setColor(Color.BLACK);
         
-        Paragraph title = new Paragraph("BILL", titlefont);
+        Paragraph title = new Paragraph("EXQUISITE BILL", titlefont);
         title.setAlignment(Paragraph.ALIGN_CENTER);
         document.add(title);
        
         
-        document.add(new Paragraph("Booking id : "+booking_id , parafont));
+        document.add(new Paragraph("Booking id : "+booking.getId(), parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Name : "+first_name+" "+last_name, parafont));
+        document.add(new Paragraph("Name : "+booking.getUser().getFirstName()+" "+booking.getUser().getLastName(), parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Email : "+user_email, parafont));
+        document.add(new Paragraph("Email : "+booking.getUser().getEmail(), parafont));
        
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Phone Number : "+user_phone, parafont));
+        document.add(new Paragraph("Phone Number : "+booking.getUser().getContactno(), parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Address : "+user_address, parafont));
+        document.add(new Paragraph("Address : "+booking.getUser().getAddress(), parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Gender : "+user_gender, parafont));
+        document.add(new Paragraph("Gender : "+booking.getUser().getGender(), parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Event :  "+event_name, parafont));
+        document.add(new Paragraph("Event :  "+booking.getEvent(), parafont));
        
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Hotel Name : "+hotel_name, parafont));
+        document.add(new Paragraph("Hotel Name : "+booking.getHotel().getHotelName(), parafont));
        
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Catering Name : "+cater_name, parafont));
+        document.add(new Paragraph("Hotel Location : "+booking.getHotel().getLocation(), parafont));
+        
+        document.add(new Paragraph(" ", parafont));
+        
+        document.add(new Paragraph("Catering Name : "+booking.getCatering().getCatername(), parafont));
        
         document.add(new Paragraph(" ", parafont));
                 
-        document.add(new Paragraph("Decorator: "+deco_name_desc, parafont));
+        document.add(new Paragraph("Catering Location : "+booking.getCatering().getCater_location(), parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Photographer: "+photo_name_desc, parafont));
+        document.add(new Paragraph("Decorator: "+booking.getDecorator_name_desc(), parafont));
+        
+        document.add(new Paragraph(" ", parafont));
+        
+        document.add(new Paragraph("Photographer: "+booking.getPhotographer_name_desc(), parafont));
        
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Disc Jockey: "+dj_name_desc ,parafont));
+        document.add(new Paragraph("Disc Jockey: "+booking.getDj_name_desc() ,parafont));
        
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Makeupartist : "+makeup_name_desc ,parafont));
+        document.add(new Paragraph("Makeupartist : "+booking.getMakeupartist_name_desc(),parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Booked on : "+booked_on, parafont));
+        document.add(new Paragraph("Booked on : "+booking.getCurrent_date(), parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Event Date: "+event_date, parafont));
+        document.add(new Paragraph("Event Date: "+booking.getEvent_date(), parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Hotel  Price : Rs."+hotel_price+" per hour", parafont));
+        document.add(new Paragraph("Hotel  Price : Rs."+booking.getHotel().getPrice()+" per hour", parafont));
        
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Event max time : "+ max_hour+" hours", parafont));
+        document.add(new Paragraph("Event max time : "+ booking.getMax_total_hour()+" hours", parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Catering Price : Rs."+cater_price+" per person", parafont));
+        document.add(new Paragraph("Catering Price : Rs."+booking.getCatering().getCater_price()+" per person", parafont));
         
         document.add(new Paragraph(" ", parafont));
         
-        document.add(new Paragraph("Number of guest : "+no_guest, parafont));
+        document.add(new Paragraph("Number of guest : "+booking.getNo_of_guest(), parafont));
         
         document.add(new Paragraph(" ", parafont));
 
         
-        document.add(new Paragraph("Total Price : Rs."+total_price, parafont));
+        document.add(new Paragraph("Total Price : Rs."+booking.getAmount(), parafont));
         
 
         
